664b8a62f4c6607cd68a661a53563f070cb18c57
u591155 <geno.503@gmail.com>
2020-09-13 19:01:50 -0700

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,58 +1,58 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    renderCampsite(campsite){
+
+function RenderCampsite({campsite}){
+    return(
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+function RenderComments({comments}){
+    if (comments){
         return(
-            <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+            <div className='col-md-5 m-1'>
+                <h4>Comments</h4>
+                {comments.map((c) => {
+                    return (
+                        <div>
+                            <p>{c.text}</p>
+                            <p>
+                                --{c.author},
+                                {new Intl.DateTimeFormat("en-US", {
+                                    year: "numeric", 
+                                    month: "short", 
+                                    day: "2-digit",
+                                    }).format(new Date(Date.parse(c.date)))}
+                            </p>
+                        </div>
+                    );
+                })}
             </div>
         );
-
     }
-
-    renderComments(comments){
-        if (comments){
-            return(
-                <div className='col-md-5 m-1'>
-                    <h4>Comments</h4>
-                    {comments.map((c) => {
-                        return (
-                            <div>
-                                <p>{c.text}</p>
-                                <p>
-                                    --{c.author},
-                                    {new Intl.DateTimeFormat("en-US", {
-                                        year: "numeric", 
-                                        month: "short", 
-                                        day: "2-digit",
-                                        }).format(new Date(Date.parse(c.date)))}
-                                </p>
-                            </div>
-                        );
-                    })}
-                </div>
-            );
-        }
-    }
+}
     
-    render(){
-        if(this.props.campsite){
-            return( 
-            <div ClassName="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
+                </div>
             </div>
-            );
-        }        
-        return <div/>;
+        );
     }
+    return <div />;
 }
 
 export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,40 +1,34 @@
-import React, { Component } from 'react';
-import App from '../App';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import CampsiteInfo from './CampsiteInfoComponent';
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-    onCampsiteSelect(campsite){
-        this.setState({selectedCampsite: campsite});
-    }
-
-    render(){
-        const directory = this.props.campsites.map(campsite => {
-            return(
-                <div key={campsite.id}  className="col-md-5 m-1">
-                    <Card onClick = {() => this.onCampsiteSelect(campsite)}>
-                        <CardImg width="100%" src = {campsite.image} alt={campsite.name}/>
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        }); 
-
-        return (
-           <div className="container"> 
-               <div className="row">{directory}</div>
-               <CampsiteInfo campsite={this.state.selectedCampsite}/>
-           </div>
-        );
-    }
+function RenderDirectoryItem({campsite, onClick}) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
 }
 
-export default Directory;  
+function Directory(props) {
+
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+            </div>
+        );
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
+}
+
+export default Directory; 
