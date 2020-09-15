417ce95bf9ec83dbb1ce27f8569cf8f71ef9e550
u591155 <geno.503@gmail.com>
2020-09-15 14:50:24 -0700

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,30 +3,39 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Router, Redirect, Route } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
         };
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
+
+
 
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">NuCamp</NavbarBrand>
-                    </div>
-                </Navbar>
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}/>}/>
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
             </div>
         );
     };
