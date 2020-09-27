a022e5fb13bdf84a421e0b4d89fefd9d355f79a2
u591155 <geno.503@gmail.com>
2020-09-27 04:03:04 -0700

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -14,6 +14,7 @@ import {
 } from "reactstrap";
 import { Control, LocalForm, Errors  } from 'react-redux-form';
 import { Link } from "react-router-dom";
+import { Loading } from './LoadingComponent';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -153,6 +154,27 @@ class CommentForm extends Component {
 }
 
 function CampsiteInfo(props) {
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	if (props.errMess) {
+		return(
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<h4>{props.errMess}</h4>
+					</div>
+				</div>
+			</div>
+		);
+	} 
+
 	if (props.campsite) {
 		return (
 			<div className='container'>
